0ad4634676e1fd698cfed201e6f402cf9fc0aa2b
David <bundgaard@users.noreply.github.com>
2021-06-13 11:35:52 +0200

    Add read method

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -15,13 +15,21 @@ func main() {
 		log.Fatalf("resolve %v", err)
 	}
 
-	socket, err := net.DialUDP("udp",nil, raddr)
+	socket, err := net.DialUDP("udp", nil, raddr)
 	if err != nil {
 		log.Fatalf("dial: %v", err)
 	}
 
+	fmt.Fprintf(socket, "Hello, World! "+time.Now().Format(time.RFC3339))
 
-	fmt.Fprintf(socket, "Hello, World! " + time.Now().Format(time.RFC3339))
+	// read blockingly the answer from server
+	buf := make([]byte, 512)
+	read, _, err := socket.ReadFrom(buf)
+	if err != nil {
+		log.Fatalf("read: %v", err)
+	}
 
+	fmt.Println("received", read, "bytes from server")
+	fmt.Println(buf[:read])
 
 }
